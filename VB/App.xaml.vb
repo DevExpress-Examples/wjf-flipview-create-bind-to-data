44c8d496cdafae3cbae8bd740afdc75cffa87c20
DevExpressExampleBot <fakeEmail>
2021-08-10 12:34:12 +0300

    Source auto update [skip ci]

--- VB/App.xaml.vb
+++ VB/App.xaml.vb
@@ -12,11 +12,6 @@ Namespace FlipViewSample
 	''' Interaction logic for App.xaml
 	''' </summary>
 	Public Partial Class App
-    		Inherits Application
-    		Protected Overrides Sub OnStartup(ByVal e As StartupEventArgs)
-        		ThemeManager.ApplicationThemeName = Theme.Office2013Name
-       		MyBase.OnStartup(e)
-    		End Sub
-End Class
-
+		Inherits Application
+	End Class
 End Namespace
